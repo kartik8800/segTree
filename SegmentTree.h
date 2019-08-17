76bfd4bd7d338775f7c226e4ba999f229dc20c1b
kartik8800 <34207988+kartik8800@users.noreply.github.com>
2019-08-17 17:17:01 +0530

    Update SegmentTree.h

--- SegmentTree.h
+++ SegmentTree.h
@@ -10,18 +10,37 @@ template<class T>
 class SegmentTree
 {
     public:
+        //tree constructors.
         SegmentTree(std::vector<T> data, T value, T (*combine)(T obj1, T obj2));
         SegmentTree(T ar[], int n, T value, T (*combine)(T obj1, T obj2));
+        
+        //query the range l to r, 0 based array indexing.
         T query(int l, int r);
+        
+        //update the element at index idx to val.
         void update(int idx, T val);
-        //TODO lazy propagation
+        ///TODO lazy propagation
     private:
+        //represents the segment tree.
         T *tree;
+    
+        //builds the segment tree.
         void buildTree(std::vector<T> data);
+        
+        //size of the segment tree array.
         int segTreeSize;
+    
+        //extra nodes must be added to array to make its size a power of 2
+        //this is the value to be filled for the those nodes.
         T valueForExtraNodes;
+    
+        //specifies how to combine child node results to form parent node result.
         T (*combine)(T obj1, T obj2);
+    
+        //used to calculate the size of array needed to store the tree.
         int calculateSize(int n);
+    
+        //helps to solve a range query.
         T queryHelper(int l,int r, int st, int ed, int node);
 };
 
@@ -49,7 +68,7 @@ template<class T> SegmentTree<T>::SegmentTree(T ar[], int n,
 }
 
 
-template<class T> SegmentTree<T>::calculateSize(int n)
+template<class T> int SegmentTree<T>::calculateSize(int n)
 {
     int pow2 = 1;
     while( pow2 < n)
@@ -105,9 +124,10 @@ template<class T> void SegmentTree<T>::update(int idx, T val)
     while(parent(segTreeIdx) >= 0)
     {
         segTreeIdx = parent(segTreeIdx);
-        tree[segTreeIdx] = combine(left(segTreeIdx), right(segTreeIdx));
+        if(right(segTreeIdx) < segTreeSize)
+          tree[segTreeIdx] = combine(tree[left(segTreeIdx)], tree[right(segTreeIdx)]);
         if(segTreeIdx == 0)
-           break;
+            break;
     }
 }
 
